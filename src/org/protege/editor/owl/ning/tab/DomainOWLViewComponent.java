dce12e4232b2e73dd879a86e04b4702acd140bdc
ning <zhun870525@126.com>
2015-02-01 10:04:09 +0800

    A basic framework to add a domain owl profile to Pretege

---
+++ src/org/protege/editor/owl/ning/tab/DomainOWLViewComponent.java
@@ -0,0 +1,33 @@
+package org.protege.editor.owl.ning.tab;
+
+import java.awt.BorderLayout;
+
+import org.apache.log4j.Logger;
+import org.protege.editor.owl.model.hierarchy.AssertedClassHierarchyProvider;
+import org.protege.editor.owl.ui.tree.OWLModelManagerTree;
+import org.protege.editor.owl.ui.tree.OWLObjectTree;
+import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
+import org.protege.owl.example.Metrics;
+import org.semanticweb.owlapi.model.OWLClass;
+
+public class DomainOWLViewComponent extends AbstractOWLViewComponent {
+    private static final long serialVersionUID = -4515710047558710080L;
+    
+    private static final Logger log = Logger.getLogger(DomainOWLViewComponent.class);
+    
+    private Metrics metricsComponent;
+
+    @Override
+    protected void initialiseOWLView() throws Exception {
+        setLayout(new BorderLayout());
+        metricsComponent = new Metrics(getOWLModelManager());
+        add(metricsComponent, BorderLayout.CENTER);
+        log.info("Example View Component initialized");
+    }
+
+	@Override
+	protected void disposeOWLView() {
+		metricsComponent.dispose();
+	}
+
+}
