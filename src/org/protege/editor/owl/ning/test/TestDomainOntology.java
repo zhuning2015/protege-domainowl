96067b5a2c70414a2bca699c221616c8c9ce63d5
ning <zhun870525@126.com>
2015-06-29 10:52:05 +0800

    Add more tests for the methods of the DomainOntology class

--- src/org/protege/editor/owl/ning/test/TestDomainOntology.java
+++ src/org/protege/editor/owl/ning/test/TestDomainOntology.java
@@ -2,6 +2,8 @@ package org.protege.editor.owl.ning.test;
 
 import org.protege.editor.owl.ning.domainOWL.DomainOntology;
 import org.protege.editor.owl.ning.domainOWL.DomainConcept;
+import org.protege.editor.owl.ning.exception.AddSameDomainConceptException;
+import org.protege.editor.owl.ning.exception.AddDomainConceptWithSameNameException;
 
 import org.junit.Test;
 import org.junit.Before;
@@ -35,6 +37,39 @@ public class TestDomainOntology
         assertTrue(domainOnt.containsDomainConcept("test"));
     }
 
+    @Test(expected=AddSameDomainConceptException.class)
+    public void TestAddDomainConcept_Same()
+    {
+        DomainConcept dc = new DomainConcept();
+        dc.setName("test");
+
+        domainOnt.addDomainConcept(dc);
+        domainOnt.addDomainConcept(dc);
+    }
+
+    @Test(expected=AddDomainConceptWithSameNameException.class)
+    public void TestAddDomainConcept_SameName()
+    {
+        DomainConcept dc = new DomainConcept();
+        dc.setName("test");
+        domainOnt.addDomainConcept(dc);
+
+        DomainConcept dc2 = new DomainConcept();
+        dc2.setName("test");
+        domainOnt.addDomainConcept(dc2);
+    }
+
+    @Test
+    public void TestGetDomainConcept()
+    {
+        DomainConcept dc = new DomainConcept();
+        dc.setName("test");
+        domainOnt.addDomainConcept(dc);
+
+        assertNotNull(domainOnt.getDomainConcept("test"));
+        assertNull(domainOnt.getDomainConcept("another"));
+    }
+
     @Test
     public void TestUpdateDomainConcept()
     {
@@ -49,6 +84,17 @@ public class TestDomainOntology
     }
 
     @Test
+    public void TestContainsDomainConcept()
+    {
+        DomainConcept dc = new DomainConcept();
+        dc.setName("test");
+        domainOnt.addDomainConcept(dc);
+
+        assertTrue(domainOnt.containsDomainConcept("test"));
+        assertFalse(domainOnt.containsDomainConcept("another"));
+    }
+
+    @Test
     public void TestRemoveDomainConcept()
     {
         DomainConcept dc = new DomainConcept();
@@ -56,6 +102,6 @@ public class TestDomainOntology
         domainOnt.addDomainConcept(dc);
 
         domainOnt.removeDomainConcept(dc);
-        assertFalse(domainOnt.containsDomainConcept(dc.getName()))
+        assertFalse(domainOnt.containsDomainConcept(dc.getName()));
     }
 }
