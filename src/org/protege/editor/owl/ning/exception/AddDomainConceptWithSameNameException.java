841aa1245df4cee0faa3c1045fb92497286381a2
ning <zhun870525@126.com>
2015-06-29 10:52:55 +0800

    Add two customized rumtime exception classes

---
+++ src/org/protege/editor/owl/ning/exception/AddDomainConceptWithSameNameException.java
@@ -0,0 +1,11 @@
+package org.protege.editor.owl.ning.exception;
+
+import java.lang.RuntimeException;
+
+public class AddDomainConceptWithSameNameException extends RuntimeException
+{
+    public AddDomainConceptWithSameNameException(String msg)
+    {
+        super(msg);
+    }
+}
---
+++ src/org/protege/editor/owl/ning/exception/AddSameDomainConceptException.java
@@ -0,0 +1,11 @@
+package org.protege.editor.owl.ning.exception;
+
+import java.lang.RuntimeException;
+
+public class AddSameDomainConceptException extends RuntimeException
+{
+    public AddSameDomainConceptException(String msg)
+    {
+        super(msg);
+    }
+}
