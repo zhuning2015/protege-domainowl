38829af851578f673303f235321b6426ffd6c426
ning <zhun870525@126.com>
2015-06-30 08:43:08 +0800

    Change AddSameDomainConceptException to AddSameOneException and change AddSameDomainConceptsException to AddOnesWithSameNameException

---
+++ src/org/protege/editor/owl/ning/exception/AddOnesWithSameNameException.java
@@ -0,0 +1,11 @@
+package org.protege.editor.owl.ning.exception;
+
+import java.lang.RuntimeException;
+
+public class AddOnesWithSameNameException extends RuntimeException
+{
+    public AddOnesWithSameNameException(String msg)
+    {
+        super(msg);
+    }
+}
---
+++ src/org/protege/editor/owl/ning/exception/AddSameOneException.java
@@ -0,0 +1,11 @@
+package org.protege.editor.owl.ning.exception;
+
+import java.lang.RuntimeException;
+
+public class AddSameOneException extends RuntimeException
+{
+    public AddSameOneException(String msg)
+    {
+        super(msg);
+    }
+}
