248ec2c2dd15b4b64190420344c128a80b5021b0
ning <zhun870525@126.com>
2015-07-03 08:41:55 +0800

    Seperating the restriction types into DATA and OBJECT two kinds

--- src/org/protege/editor/owl/ning/domainOWL/RestrictionType.java
+++ src/org/protege/editor/owl/ning/domainOWL/RestrictionType.java
@@ -9,9 +9,54 @@ package org.protege.editor.owl.ning.domainOWL;
  */
 public enum RestrictionType
 {
-    SOME,    //existential
-    ONLY,    //universal
-    MIN,     //min cardinality
-    EXACTLY, //exact cardinality
-    MAX      //max cardinality
+    /**
+     * An existential class expression, corresponding to
+     * DataSomeValuesFrom in OWL2
+     */
+    SOME_DATA,
+    /**
+     * A universal class expression, corresponding to
+     * DataAllValuesFrom in OWL2
+     */
+    ONLY_DATA,
+    /**
+     * A minimum cardinality expression, corresponding to
+     * DataMinCardinality in OWL2
+     */
+    MIN_DATA,
+    /**
+     * An exact cardinality expression, corresponding to
+     * DataExactCardinality in OWL2
+     */
+    EXACTLY_DATA,
+    /**
+     * A maximum cardinality expression, corresponding to
+     * DataMaxCardinality in OWL2
+     */
+    MAX_DATA,
+    /**
+     * An existential class expression, corresponding to
+     * ObjectSomeValuesFrom in OWL2
+     */
+    SOME_OBJECT,
+    /**
+     * A universal class expression, corresponding to
+     * ObjectAllValuesFrom in OWL2
+     */
+    ONLY_OBJECT,
+    /**
+     * A minimum cardinality expression, corresponding to
+     * ObjectMinCardinality in OWL2
+     */
+    MIN_OBJECT,
+    /**
+     * An exact cardinality expression, corresponding to
+     * ObjectExactCardinality in OWL2
+     */
+    EXACTLY_OBJECT,
+    /**
+     * A maximum cardinality expression, corresponding to
+     * ObjectMaxCardinality in OWL2
+     */
+    MAX_OBJECT
 }
