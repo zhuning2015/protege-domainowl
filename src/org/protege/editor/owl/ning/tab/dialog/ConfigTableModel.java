ad26dcb87d047cf6274b8c5f201a50479fe93633
ning <zhun870525@126.com>
2015-07-04 19:23:38 +0800

    Divide the original table into three different tables for different types of the meta ontology elements

--- src/org/protege/editor/owl/ning/tab/dialog/ConfigTableModel.java
+++ src/org/protege/editor/owl/ning/tab/dialog/ConfigTableModel.java
@@ -6,17 +6,49 @@ import org.protege.editor.owl.ning.exception.BasicException;
 
 import javax.swing.table.AbstractTableModel;
 
-public class ConfigTableModel extends AbstractTableModel
+/**
+ * The configure table model for the tables in the domain owl
+ * configure dialog by customizing AbstractTableModel
+ *
+ * @author Zhu Ning
+ * @version 0.1.0
+ */
+public abstract class ConfigTableModel extends AbstractTableModel
 {
+    /**
+     * The column number of the table
+     */
     private final int COLUMN_COUNT = 3;
 
+    /**
+     * The column names of the table
+     */
     String[] columnNames = new String[]{"Name", "IsIncluded", "Icon"};
 
-    private void checkColumnCount(int column)
+    /**
+     * Checks if the column index is inside the scope of the max
+     * column count constrainted by COLUMN_COUNT
+     * @param column The column index to check
+     * @exception BasicException Throws when the column index is not
+     * less than the COLUMN_COUNT
+     */
+    protected void checkColumnCount(int column)
     {
         if (column >= COLUMN_COUNT)
             throw new BasicException
                 ("The column number in the table is not right");
+    }
+
+    /**
+     * Gets the meta ontology element (meta concept, meta relation and
+     * instance) according to the row index in the table. It needs to
+     * be overrided by different table models
+     * @param rowIndex The row index of the meta ontology element is in
+     * @return The meta ontology element in the row index rowIndex
+     */
+    protected MetaOntologyElement getMetaOntologyElement(int rowIndex)
+    {
+        return null;
     }
 
     @Override
@@ -32,45 +64,6 @@ public class ConfigTableModel extends AbstractTableModel
         return columnNames[column];
     }
 
-    @Override
-    public int getRowCount()
-    {
-        MetaOntology metaOnt = MetaOntology.getMetaOntology();
-        int rowCount =  metaOnt.getMetaConceptCount() +
-            metaOnt.getMetaRelationCount() +
-            metaOnt.getInstanceCount();
-        return rowCount;
-    }
-
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex)
-    {
-        checkColumnCount(columnIndex);
-        MetaOntology metaOnt = MetaOntology.getMetaOntology();
-        MetaOntologyElement moe = null;
-
-        int metaCncptCount = metaOnt.getMetaConceptCount();
-        int metaRelationCount = metaOnt.getMetaRelationCount();
-
-        if (rowIndex < metaCncptCount)
-        {
-            moe = metaOnt.getMetaConcept(rowIndex);
-        }else if (rowIndex - metaCncptCount < metaRelationCount)
-        {
-            moe = metaOnt.getMetaRelation(rowIndex - metaCncptCount);
-        }else
-        {
-            moe = metaOnt.getInstance(rowIndex - metaCncptCount -
-                                      metaRelationCount);
-        }
-
-        if (columnIndex == 0)
-            return moe.getName();
-        else if (columnIndex == 1)
-            return moe.getIsIncluded();
-        else
-            return moe.getImagePath();
-    }
 
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex)
@@ -84,5 +77,103 @@ public class ConfigTableModel extends AbstractTableModel
     public Class getColumnClass(int c)
     {
         return getValueAt(0, c).getClass();
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex)
+    {
+        checkColumnCount(columnIndex);
+        MetaOntologyElement moe = getMetaOntologyElement(rowIndex);
+
+        if (columnIndex == 0)
+            return moe.getName();
+        else if (columnIndex == 1)
+            return moe.getIsIncluded();
+        else
+            return moe.getImagePath();
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex)
+    {
+        checkColumnCount(columnIndex);
+        MetaOntology metaOnt = MetaOntology.getMetaOntology();
+        MetaOntologyElement moe = getMetaOntologyElement(rowIndex);
+
+        if (columnIndex == 1)
+            moe.setIsIncluded(!moe.getIsIncluded());
+
+        else if (columnIndex == 2)
+            moe.setImagePath((String)aValue);
+    }
+
+}
+
+/**
+ * The table model for the table listing meta concepts
+ *
+ * @author Zhu Ning
+ * @version 0.1.0
+ */
+class MetaConceptConfigTableModel extends ConfigTableModel
+{
+    @Override
+    public int getRowCount()
+    {
+        MetaOntology metaOnt = MetaOntology.getMetaOntology();
+        return metaOnt.getMetaConceptCount();
+    }
+
+    @Override
+    protected MetaOntologyElement getMetaOntologyElement(int rowIndex)
+    {
+        MetaOntology metaOnt = MetaOntology.getMetaOntology();
+        return metaOnt.getMetaConcept(rowIndex);
+    }
+}
+
+/**
+ * The table model for the table listing meta relations
+ *
+ * @author Zhu Ning
+ * @version 0.1.0
+ */
+class MetaRelationConfigTableModel extends ConfigTableModel
+{
+    @Override
+    public int getRowCount()
+    {
+        MetaOntology metaOnt = MetaOntology.getMetaOntology();
+        return metaOnt.getMetaRelationCount();
+    }
+
+    @Override
+    protected MetaOntologyElement getMetaOntologyElement(int rowIndex)
+    {
+        MetaOntology metaOnt = MetaOntology.getMetaOntology();
+        return metaOnt.getMetaRelation(rowIndex);
+    }
+}
+
+/**
+ * The table model for the table listing instances
+ *
+ * @author Zhu Ning
+ * @version 0.1.0
+ */
+class InstanceConfigTableModel extends ConfigTableModel
+{
+    @Override
+    public int getRowCount()
+    {
+        MetaOntology metaOnt = MetaOntology.getMetaOntology();
+        return metaOnt.getInstanceCount();
+    }
+
+    @Override
+    protected MetaOntologyElement getMetaOntologyElement(int rowIndex)
+    {
+        MetaOntology metaOnt = MetaOntology.getMetaOntology();
+        return metaOnt.getInstance(rowIndex);
     }
 }
--- src/org/protege/editor/owl/ning/tab/dialog/DomainOWLPanelConfigureDlg.java
+++ src/org/protege/editor/owl/ning/tab/dialog/DomainOWLPanelConfigureDlg.java
@@ -5,6 +5,12 @@ import org.protege.editor.owl.ning.domainOWL.MetaOntology;
 import javax.swing.JDialog;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+//import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 /**
  * The dialog to configure the domain owl panel to decide which
@@ -17,23 +23,77 @@ import javax.swing.JScrollPane;
 public class DomainOWLPanelConfigureDlg extends JDialog
 {
     /**
-     * The current meta ontology
+     * The types for the tables shown in the configure dialog
      */
-    private MetaOntology metaOnt = null;
-
-    /**
-     * The configure table for configure the domain owl panel
-     */
-    private JTable configTable = null;
-
-    public DomainOWLPanelConfigureDlg(MetaOntology metaOnt)
+    private enum TableType
     {
-        this.metaOnt = metaOnt;
-        configTable = new JTable(new ConfigTableModel());
-        add(new JScrollPane(configTable));
+        METACONCEPT_TABLE,
+        METARELATION_TABLE,
+        INSTANCE_TABLE
+    }
+
+    public DomainOWLPanelConfigureDlg()
+    {
+        setLayout(new BorderLayout());
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(2,2));
+
+        JPanel metaCncptPanel =
+            createSubConfigPanel(TableType.METACONCEPT_TABLE);
+        JPanel metaRelationPanel =
+            createSubConfigPanel(TableType.METARELATION_TABLE);
+        JPanel instancePanel =
+            createSubConfigPanel(TableType.INSTANCE_TABLE);
+
+        mainPanel.add(metaCncptPanel);
+        mainPanel.add(metaRelationPanel);
+        mainPanel.add(instancePanel);
+
+        JPanel buttonPanel = new JPanel();
+        //        JCheckBox checkBox =
+        //            new JCheckBox("Not show the dialog next time");
+        JButton okBtn = new JButton("OK");
+        //        buttonPanel.add(checkBox);
+        buttonPanel.add(okBtn);
+
+        add(mainPanel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
+
         pack();
     }
 
+    /**
+     * Create a sub config panel according to different type of meta
+     * tables including MetaConcept Table, MetaRelation Table and
+     * Instance Table
+     * @param tableType The type of the table
+     * @return The sub configure panel for the tableType-type table
+     */
+    private JPanel createSubConfigPanel(TableType tableType)
+    {
+        String title = null;
+        JTable table = null;
 
+        switch (tableType)
+        {
+        case METACONCEPT_TABLE:
+            title = "Meta Concepts:";
+            table = new JTable(new MetaConceptConfigTableModel());
+            break;
+        case METARELATION_TABLE:
+            title = "Meta Relations:";
+            table = new JTable(new MetaRelationConfigTableModel());
+            break;
+        case INSTANCE_TABLE:
+            title = "Instances:";
+            table = new JTable(new InstanceConfigTableModel());
+        }
 
+        JPanel configPanel = new JPanel();
+        configPanel.setBorder(BorderFactory.createTitledBorder(title));
+        configPanel.add(new JScrollPane(table));
+
+        return configPanel;
+    }
 }
