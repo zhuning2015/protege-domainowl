9ddb7fabc863aae3b060cf1b83585cc892108df0
ning <zhun870525@126.com>
2015-07-07 21:00:14 +0800

    Add annotations for the overrided methods to denotate they are overrided

--- src/org/protege/editor/owl/ning/tab/dialog/MetaConceptListModel.java
+++ src/org/protege/editor/owl/ning/tab/dialog/MetaConceptListModel.java
@@ -5,25 +5,36 @@ import org.protege.editor.owl.ning.domainOWL.MetaOntology;
 import javax.swing.ListModel;
 import javax.swing.event.ListDataListener;
 
+/**
+ * The customized list model for the meta model list used to
+ * creating domain concepts
+ *
+ * @author Zhu Ning
+ * @version 0.1.0
+ */
 public class MetaConceptListModel implements ListModel
 {
 
+    @Override
     public int getSize()
     {
         MetaOntology metaOnt = MetaOntology.getMetaOntology();
         return metaOnt.getSelectedMetaConceptsCount();
     }
 
+    @Override
     public Object getElementAt(int index)
     {
         MetaOntology metaOnt = MetaOntology.getMetaOntology();
         return metaOnt.getSelectedMetaConcept(index);
     }
 
+    @Override
     public void addListDataListener(ListDataListener l)
     {
     }
 
+    @Override
     public void removeListDataListener(ListDataListener l)
     {
     }
