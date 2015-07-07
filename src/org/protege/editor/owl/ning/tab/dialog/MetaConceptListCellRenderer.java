ac52f816936c60ec46821ffef42dba1d64aae913
ning <zhun870525@126.com>
2015-07-07 20:59:16 +0800

    Add the annotations and Using FontMetrics class to get the width of the string to locate the string in a good position

--- src/org/protege/editor/owl/ning/tab/dialog/MetaConceptListCellRenderer.java
+++ src/org/protege/editor/owl/ning/tab/dialog/MetaConceptListCellRenderer.java
@@ -7,20 +7,44 @@ import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
 import javax.swing.ImageIcon;
 import javax.swing.JList;
+
 import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Dimension;
-
 import java.awt.Color;
+import java.awt.FontMetrics;
 
+/**
+ * The customized list cell renderer for the meta concept list
+ * in the domain owl panel for customzing domain concepts
+ *
+ * @author Zhu Ning
+ * @version 0.1.0
+ */
 public class MetaConceptListCellRenderer extends JPanel
     implements ListCellRenderer
 {
+    /**
+     * The image icon for the list cell
+     */
     private ImageIcon icon;
+
+    /**
+     * The name of the list cell representing meta concept
+     */
     private String name;
+
+    /**
+     * The background of the panel for the list cell
+     */
     private Color background;
+
+    /**
+     * The foreground of the panel for the list cell
+     */
     private Color foreground;
 
+    @Override
     public Component getListCellRendererComponent(JList list,
                                                   Object value,
                                                   int index,
@@ -40,6 +64,7 @@ public class MetaConceptListCellRenderer extends JPanel
         return this;
     }
 
+    @Override
     public void paint(Graphics g)
     {
         int imageWidth = icon.getImage().getWidth(null);
@@ -48,11 +73,14 @@ public class MetaConceptListCellRenderer extends JPanel
         g.fillRect(0, 0, getWidth(), getHeight());
         g.setColor(foreground);
         g.drawImage(icon.getImage(), getWidth() / 2 - imageWidth / 2,
-                    32, null);
-        g.drawString(name, getWidth() / 2 - name.length() / 2,
-                     imageHeight + 40);
+                    10, null);
+        FontMetrics metrics = g.getFontMetrics(g.getFont());
+
+        g.drawString(name, getWidth() / 2 - metrics.stringWidth(name) / 2,
+                     imageHeight + 20);
     }
 
+    @Override
     public Dimension getPreferredSize()
     {
         return new Dimension(60,80);
