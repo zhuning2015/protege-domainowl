0b76e17162023e9a1b6f5c5a4a99edf3faaf5364
ning <zhun870525@126.com>
2015-07-09 20:39:39 +0800

    Add the package and related classes for jgraph

---
+++ src/org/protege/editor/owl/ning/tab/graph/DomainViewGraphModel.java
@@ -0,0 +1,13 @@
+package org.protege.editor.owl.ning.tab.graph;
+
+import org.jgraph.graph.DefaultGraphModel;
+
+/**
+ * The customized graph model for the domain view panel
+ *
+ * @author Zhu Ning
+ * @version 0.1.0
+ */
+public class DomainViewGraphModel extends DefaultGraphModel
+{
+}
