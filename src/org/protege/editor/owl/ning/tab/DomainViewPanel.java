16cae1e1bccae46f74bada25e378221dd898ab28
ning <zhun870525@126.com>
2015-07-09 20:38:13 +0800

    Modify the DomainViewPanel inheriting from JGraph directly

--- src/org/protege/editor/owl/ning/tab/DomainViewPanel.java
+++ src/org/protege/editor/owl/ning/tab/DomainViewPanel.java
@@ -1,15 +1,43 @@
 package org.protege.editor.owl.ning.tab;
 
+import org.protege.editor.owl.ning.tab.graph.DomainViewGraphModel;
+import org.protege.editor.owl.ning.domainOWL.DomainConcept;
+
 import org.jgraph.JGraph;
+import org.jgraph.graph.GraphModel;
+import org.jgraph.graph.GraphLayoutCache;
+import org.jgraph.graph.DefaultGraphCell;
+import org.jgraph.graph.GraphConstants;
 
 import javax.swing.JScrollPane;
-import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 
-public class DomainViewPanel extends JPanel
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * The right panel in the domain owl panel for visualizing the domain
+ * concepts and relations in the domain ontology
+ *
+ * @author Zhu Ning
+ * @version 0.1.0
+ */
+public class DomainViewPanel extends JGraph
 {
-    JGraph graph = new JGraph();
-    public DomainViewPanel()
+    public DomainViewPanel(GraphModel model, GraphLayoutCache view)
     {
-        add(new JScrollPane(graph));
+        super(model, view);
+    }
+
+    public void addCell(DomainConcept dc)
+    {
+        DefaultGraphCell cell = new DefaultGraphCell(dc);
+        GraphConstants.setBounds(cell.getAttributes(), new Rectangle2D.Double(20,20,40,20));
+        GraphConstants.setGradientColor(cell.getAttributes(), Color.orange);
+        GraphConstants.setOpaque(cell.getAttributes(), true);
+        getGraphLayoutCache().insert(cell);
+        getGraphLayoutCache().setVisible(cell, true);
+        getGraphLayoutCache().update();
     }
 }
